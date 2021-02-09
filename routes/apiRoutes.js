b749c87fddecf02b3e4c4945db19d1b3cf388fe2
phamthomas12 <phamthomas12@gmail.com>
2021-02-09 19:20:18 +1100

    added serve.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const db = require('../db/db.json');
+
+
+module.exports = (app) => {
+
+  let notesData = [];
+
+  
+  app.get("/api/notes", function(err, res) {
+    try {
+      notesData = fs.readFileSync(`${__dirname}/../db/db.json`, "utf8");
+      notesData = JSON.parse(notesData);
+    } catch (err) {
+      console.log("\n error:");
+      console.log(error);
+    }
+    res.json(notesData);
+  });
+
+
+
+  app.post("/api/notes", function(req, res) {
+    try {
+      notesData = fs.readFileSync(`${__dirname}/../db/db.json`, "utf8");
+      notesData = JSON.parse(notesData);
+      req.body.id = notesData.length;
+      notesData.push(req.body); 
+      notesData = JSON.stringify(notesData);
+    
+      fs.writeFile(`${__dirname}/../db/db.json`, notesData, "utf8", function(err) {
+        if (err) 
+          throw err;
+      });
+
+    res.json(JSON.parse(notesData));
+    } catch (err) {
+        throw err;
+        console.log(error);
+    }
+  });
+
+
+
+app.delete("/api/notes/:id", function(req, res) {
+  try {
+    notesData = fs.readFileSync(`${__dirname}/../db/db.json`, "utf8");
+    notesData = JSON.parse(notesData);
+    notesData = notesData.filter(function(note) {
+      return note.id != req.params.id;
+    });
+    notesData = JSON.stringify(notesData);
+    fs.writeFile(`${__dirname}/../db/db.json`, notesData, "utf8", function(err) {
+      if (err) throw err;
+    });
+    
+    res.send(JSON.parse(notesData));
+  } catch (err) {
+    throw err;
+    console.log(err);
+  }
+});
+
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+// EXPRESS CONFIGURATION
+const app = express();
+
+// Set up the Middleware
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// Point the server towards the "route" files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// LISTENER
+// Define dynamic port
+const PORT = process.env.PORT || 3030;
+
+// Start the server to begin listening
+app.listen(PORT,() => {
+    console.log(`I am listening on PORT ${PORT}`);
+});
